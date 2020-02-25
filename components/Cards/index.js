f364e71af913d22d52aa6414d2597ca969d03df3
Kirk Tolliver <kirktolliver28@gmail.com>
2020-02-24 18:55:08 -0600

    Final push last component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,62 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardContainer = document.querySelector('.cards-container');
+
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then((response) => {
+        let apiData = response.data.articles;
+        let bootstrapArr = apiData.bootstrap;
+        let javascriptArr = apiData.javascript;
+        let technologyArr = apiData.technology;
+        let jqueryArr = apiData.jquery;
+        let nodeArr = apiData.node;
+
+        function cardDisplay(arr) {
+            arr.forEach((item) => {
+                cardContainer.appendChild(cardCreator(item));
+            });
+        }
+
+        cardDisplay(bootstrapArr);
+        cardDisplay(javascriptArr);
+        cardDisplay(technologyArr);
+        cardDisplay(jqueryArr);
+        cardDisplay(nodeArr);
+    });
+
+
+
+
+function cardCreator(tab) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const headLine = document.createElement('div');
+    headLine.classList.add('headline');
+    headLine.textContent = tab.headline;
+    card.appendChild(headLine);
+
+    const author = document.createElement('div');
+    author.classList.add('author');
+
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');
+
+
+    const img = document.createElement('img');
+    img.src = tab.authorPhoto;
+    imgContainer.appendChild(img);
+
+    const name = document.createElement('span');
+    name.textContent = `By ${tab.authorName}`;
+    author.appendChild(imgContainer);
+    author.appendChild(name);
+
+    card.appendChild(author);
+
+    return card;
+}
